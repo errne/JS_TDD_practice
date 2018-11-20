2671136d49a50cc4d21f49a78ff77f7dcb6bf5dd
Ernest Fraiman <upapaus@gmail.com>
2018-11-20 17:15:47 +0000

    tests passed

--- models/park.js
+++ models/park.js
@@ -4,4 +4,22 @@ const Park = function (name, ticketPrice) {
   this.dinosaurs = [];
 }
 
+Park.prototype.addDino = function (dinosaur) {
+  this.dinosaurs.push(dinosaur);
+};
+
+Park.prototype.removeDino = function (dinosaur) {
+  const index = this.dinosaurs.indexOf(dinosaur);
+  if (index >= 0) this.dinosaurs.splice(index, 1);
+};
+
+Park.prototype.mostAttractiveDino = function () {
+  let mostAttractiveDino;
+  for (const dino of this.dinosaurs){
+    if (dino.guestsAttractedPerDay > mostAttractiveDino.guestsAttractedPerDay)
+      mostAttractiveDino = dino;
+  }
+  return mostAttractiveDino;
+};
+
 module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -4,8 +4,12 @@ const Dinosaur = require('../models/dinosaur.js');
 
 describe('Park', function() {
   let park;
+  let dino1;
   beforeEach(function () {
     park = new Park("Fun Park", 25);
+    dino1 = new Dinosaur('t-rex', 'carnivore', 50);
+    dino2 = new Dinosaur('aardonyx', 'omnivore', 45);
+    dino3 = new Dinosaur('raptor', 'herbivore', 76);
   });
 
   it('should have a name', function () {
@@ -19,17 +23,32 @@ describe('Park', function() {
   });
 
   it('should have a collection of dinosaurs', function () {
-      const actual = park.dinosaurs;
-      assert.deepStrictEqual(actual, [])
+    const actual = park.dinosaurs;
+    assert.deepStrictEqual(actual, [])
   });
 
+  it('should be able to add a dinosaur to its collection', function () {
+    park.addDino(dino1);
+    const actual = park.dinosaurs;
+    assert.deepStrictEqual(actual, [dino1]);
+  });
 
+  it('should be able to remove a dinosaur from its collection', function () {
+    park.addDino(dino1);
+    park.addDino(dino2);
+    park.removeDino(dino2);
+    const actual = park.dinosaurs;
+    assert.deepStrictEqual(actual, [dino1]);
+  });
 
-  it('should be able to add a dinosaur to its collection');
+  it('should be able to find the dinosaur that attracts the most visitors', function () {
+    park.addDino(dino1);
+    park.addDino(dino2);
+    park.addDino(dino3);
+    const actual = park.mostAttractiveDino();
+    assert.deepStrictEqual(actual, dino3);
+  });
 
-  it('should be able to remove a dinosaur from its collection');
-
-  it('should be able to find the dinosaur that attracts the most visitors');
 
   it('should be able to find all dinosaurs of a particular species');
 
